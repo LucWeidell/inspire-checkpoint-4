068c3f0b300593cd3b152dd25d16186f7aa59190
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-07-23 17:30:55 -0600

    img to large + author added

--- app/Controllers/BgsController.js
+++ app/Controllers/BgsController.js
@@ -3,7 +3,8 @@ import { sandApiImages } from "../Services/AxiosService.js";
 async function _draw(){
   try{
   const res = await sandApiImages.get()
-  document.body.style.backgroundImage = `"url('${res.data.imgURL}')"`
+  document.body.style.backgroundImage = `"url('${res.data.largeImgUrl}')"`
+  document.getElementById('bg-auth').innerText = res.data.author
   } catch (error){
     console.log('Er: getting bg-img')
   }
