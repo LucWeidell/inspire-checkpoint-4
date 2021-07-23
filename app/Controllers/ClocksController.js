d331fee8e00b1156e3aa9f5cf44b7b907024b13c
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-07-23 16:52:07 -0600

    clock controller written

---
+++ app/Controllers/ClocksController.js
@@ -0,0 +1,29 @@
+
+function _updateClock12(){
+  let template = ''
+  let currentTime = new Date()
+  template += /*html*/ `
+    <h1>${currentTime.toLocaleTimeString()}</h1>
+    <h3>Good Day, User</h3>
+  `
+  document.getElementById('clock').innerHTML = template
+
+}
+
+// NOTE can add this for day status
+// function _getDayStatus(time){
+//   if(time.getHours() > )
+// }
+
+export default class ClocksController{
+
+  constructor(){
+    this.clock = setInterval(_updateClock12, 60000)
+  }
+
+  //NOTE can write a update _updateClock24
+  //It would clear the this.clock and set it setInterval(_updateClock24, 60000)
+
+
+
+}
